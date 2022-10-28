e9ac95ccf454b5a8b3a30db4605e16c883da2759
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2022-10-28 18:16:59 -0500

    Build rainbow functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,27 @@
 const containerEle = document.querySelector(".container");
 const tileInputEle = document.querySelector("#number-of-tiles");
+const rainbowEle = document.querySelector("#rainbow");
 const tileBtn = document.querySelector("#tile-btn");
 
 function clearBoard() {
   containerEle.innerHTML = "";
+}
+
+function calculateRandomValue() {
+  return Math.floor(Math.random() * 256);
+}
+
+function colorTilesHandler() {
+  const isRainbowChecked = rainbowEle.checked;
+
+  if (isRainbowChecked) {
+    const redValue = calculateRandomValue();
+    const greenValue = calculateRandomValue();
+    const blueValue = calculateRandomValue();
+    this.style.backgroundColor = `rgb(${redValue}, ${greenValue}, ${blueValue})`;
+  } else {
+    this.classList.add("black-square");
+  }
 }
 
 function populateContainer(numOfTiles) {
@@ -23,9 +41,7 @@ function populateContainer(numOfTiles) {
   for (let i = 0; i <= totalTiles; i++) {
     const squareEle = document.createElement("div");
     containerEle.appendChild(squareEle);
-    squareEle.addEventListener("mouseover", () => {
-      squareEle.classList.add("black-square");
-    });
+    squareEle.addEventListener("mouseover", colorTilesHandler);
   }
 }
 
